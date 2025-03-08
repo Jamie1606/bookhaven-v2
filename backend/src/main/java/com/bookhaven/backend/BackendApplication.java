4668059c8f1094f1e4752843cc8eaa4a2c9b6444
Zay Yar Tun <zyt.tharthar@gmail.com>
2025-03-08 14:42:46 +0800

    setup

---
+++ backend/src/main/java/com/bookhaven/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.bookhaven.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
